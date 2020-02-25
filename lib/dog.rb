2378ce9c69dc39ee61c0d044a28c8855495d57cc
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-25 02:53:40 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(dog_name)
+    this_pups_name = dog_name
+  end
+  def name
+    this_pups_name
+  end
+  def breed=(dog_breed)
+    this_pups_breed = dog_breed
+  end
+  def breed
+    this_pups_breed
+  end
+end
